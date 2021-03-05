30f7f25f08997abb14f26f1d68a750e29b7a1615
Silas <baw4416@uni-hamburg.de>
2021-03-05 11:09:21 +0100

    Sentiment Analyse

--- backend-server-master/src/main/java/basecamp/project/server/controller/UserController.java
+++ backend-server-master/src/main/java/basecamp/project/server/controller/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import basecamp.project.server.functions.NLP;
+
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -25,12 +28,12 @@ public class UserController {
 		if(hashtag.replace(" ", "").equals("blm"))
 		{
 				test = new word[] { new word("Police", 80.5f), new word("Black", 25.6f), new word("Matters", 14.2f),
-				new word("Love", 8.7f), new word("hate", 4.7f), new word("America", 1.7f), new word("Trump", 1.4f),
-				new word("Sad", 0.7f), new word("Black", 25.6f), new word("Matters", 14.2f), new word("Love", 8.7f),
+				new word("dumb", 8.7f), new word("hate", 4.7f), new word("America", 1.7f), new word("Trump", 1.4f),
+				new word("Sad", 0.7f), new word("Black", 25.6f), new word("Matters", 14.2f), new word("dumb", 8.7f),
 				new word("hate", 4.7f), new word("America", 1.7f), new word("Trump", 1.4f), new word("Sad", 0.7f) };
 		}else{
-			test = new word[] { new word("Woman",23.5f), new word("Man", 95.6f), new word("Sad", 64.2f),
-				new word("Happy", 67.7f), new word("Together", 2.7f), new word("small", 9.7f), new word("Big", 17.4f)};
+			test = new word[] { new word("dumb",23.5f), new word("love", 95.6f), new word("Sad", 64.2f),
+				new word("Happy", 67.7f), new word("Together", 2.7f), new word("kill", 9.7f), new word("hate", 17.4f)};
 		}
 		
 		for (word w : test) {
@@ -40,8 +43,20 @@ public class UserController {
 			words.add(target);
 		}
 
-		
+		NLP.init();
+		float sentiment = 0f;
+		float perc = 0f;
+		for(word w : test)
+		{
+			sentiment += (NLP.findSentiment(w.wo))*w.percentage;
+			perc += w.percentage;
+		}
+
+		System.out.println(sentiment/perc);
+		sentiment /= perc;
+
 		www.put("words",words);
+		www.put("sentiment", sentiment);
 		return www.toJSONString();
 	}
 
